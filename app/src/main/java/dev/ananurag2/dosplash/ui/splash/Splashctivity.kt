9e23c9a434cecfe560e8c1bb9ddd389f8a092b63
ankuranurag2 <ankuranurag2@gmail.com>
2020-10-21 10:35:50 +0530

    Splash Activity class added, minor code refactoring

---
+++ app/src/main/java/dev/ananurag2/dosplash/ui/splash/Splashctivity.kt
@@ -0,0 +1,11 @@
+package dev.ananurag2.dosplash.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splashctivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashctivity)
+    }
+}
