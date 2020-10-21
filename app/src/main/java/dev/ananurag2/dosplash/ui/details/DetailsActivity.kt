c47b1ef4dc61471a6dcd0bd07ce9ce91dffff32b
ankuranurag2 <ankuranurag2@gmail.com>
2020-10-21 20:09:17 +0530

    Details Activity added, layout for details

---
+++ app/src/main/java/dev/ananurag2/dosplash/ui/details/DetailsActivity.kt
@@ -0,0 +1,11 @@
+package dev.ananurag2.dosplash.ui.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
