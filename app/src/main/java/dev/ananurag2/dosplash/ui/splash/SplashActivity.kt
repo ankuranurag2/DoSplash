1b55796600de18bdb818f417061e328729c83ab5
ankuranurag2 <ankuranurag2@gmail.com>
2020-10-21 10:38:00 +0530

    spellling correction in filename

--- app/src/main/java/dev/ananurag2/dosplash/ui/splash/SplashActivity.kt
+++ app/src/main/java/dev/ananurag2/dosplash/ui/splash/SplashActivity.kt
@@ -2,10 +2,11 @@ package dev.ananurag2.dosplash.ui.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.ananurag2.dosplash.R
 
-class Splashctivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splashctivity)
+        setContentView(R.layout.activity_splash)
     }
 }
