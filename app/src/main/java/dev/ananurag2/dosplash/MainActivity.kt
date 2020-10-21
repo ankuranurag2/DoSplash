1ba9cdc182a647626f310587933a69f9ea2a3ede
ankuranurag2 <ankuranurag2@gmail.com>
2020-10-21 10:26:40 +0530

    initial commit with IDE genrated code

---
+++ app/src/main/java/dev/ananurag2/dosplash/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.ananurag2.dosplash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
